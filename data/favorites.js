131efd80bc4eac5697e74329140b05f89aced5fd
harriet-the-spy <dpcairns@gmail.com>
2020-11-12 14:45:18 -0800

    initial commit

---
+++ data/favorites.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    title: 'bessie',
+    movie_api_id: 550,
+  },
+  {
+    title: 'jumpy',
+    movie_api_id: 650,
+  },
+  {
+    title: 'spot',
+    movie_api_id: 750,
+  }
+];
---
+++ data/load-seed-data.js
@@ -0,0 +1,47 @@
+const client = require('../lib/client');
+// import our seed data:
+const favorites = require('./favorites.js');
+const usersData = require('./users.js');
+const { getEmoji } = require('../lib/emoji.js');
+
+run();
+
+async function run() {
+
+  try {
+    await client.connect();
+
+    const users = await Promise.all(
+      usersData.map(user => {
+        return client.query(`
+                      INSERT INTO users (email, hash)
+                      VALUES ($1, $2)
+                      RETURNING *;
+                  `,
+        [user.email, user.hash]);
+      })
+    );
+      
+    const user = users[0].rows[0];
+
+    await Promise.all(
+      favorites.map(favorite => {
+        return client.query(`
+                    INSERT INTO favorites (movie_api_id, title, owner_id)
+                    VALUES ($1, $2, $3);
+                `,
+        [favorite.movie_api_id, favorite.title, user.id]);
+      })
+    );
+    
+
+    console.log('seed data load complete', getEmoji(), getEmoji(), getEmoji());
+  }
+  catch(err) {
+    console.log(err);
+  }
+  finally {
+    client.end();
+  }
+    
+}
